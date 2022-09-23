587e7ca49782ae97779c2f5f087a97ab4a804537
NMD-haru <111827134+NMD-haru@users.noreply.github.com>
2022-09-23 19:48:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+$errors= array();
+   if(isset($_FILES['image'])){
+      $file_name = $_FILES['image']['name'];
+      $file_size = $_FILES['image']['size'];
+      $file_tmp = $_FILES['image']['tmp_name'];
+      $file_type = $_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("jpeg","jpg","png","gif");
+      echo $_FILES['image']['name'];
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG,JPG,PNG or GIF file.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         move_uploaded_file($file_tmp,"TEMP/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+    }
+?>
+<style type="text/css">
+    #display-image{
+    max-width: 40%;
+    max-height: 40%;
+    justify-content: center;
+    padding: 5px;
+    margin: 15px;
+}
+</style>
+<html>
+   <body>
+      
+      <form action = "" method = "POST" enctype = "multipart/form-data">
+         <input type = "file" name = "image"  onchange="form.submit()" />
+			
+         <ul>
+            <li>Sent file: <?php echo $file_name  ?>
+            <li>File size: <?php echo $_FILES['image']['size'];  ?>
+            <li>File type: <?php echo $_FILES['image']['type'] ?>
+         </ul>
+      </form>
+      <img id="display-image" src="./TEMP/<?php echo $_FILES['image']['name']; ?>">
+   </body>
+</html>
